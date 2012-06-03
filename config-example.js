2b55b73d1b96b06d2640f8353acecb67a3f8b12e
Matt Bell <matt@safeb.it>
2012-06-02 18:43:58 -0700

    Added dynamic page loading

--- config-example.js
+++ config-example.js
@@ -3,11 +3,17 @@ var config = {};
 config.port = 80; // the port to listen on for user connection
 	
 config.master = 'localhost'; // the master server (which scrapes HN and renders site images)
-config.dbUser = 'mongoUser';
-config.dbPadd = '1234',
+config.dbUser = 'dbUser';
+config.dbPadd = '12345',
 config.db = 'hn2';
+
+config.mode = 'master';
+
+config.frontPageMaxAge = 24 * 60 * 60 * 1000;
+config.timeDecay = 0.0018;
+config.pointPower = 2.1;
 	
-config.cacheAge = 7 * 24 * 60 * 60 * 1000; // the maxAge for web resources
+config.cacheAge = 0;//7 * 24 * 60 * 60 * 1000; // the maxAge for web resources
 	
 config.scraper = {
 	frontInterval: 10 * 1000, // how often to update front page posts
--- public/js/hn2.js
+++ public/js/hn2.js
@@ -1,4 +1,12 @@
-var Item = Backbone.Model.extend({});
+var Item = Backbone.Model.extend({
+	initialize: function() {
+		_.bindAll(this, 'remove');
+	},
+	remove: function() {
+		this.trigger('remove');
+		delete this;
+	}
+});
 	
 var Items = Backbone.Collection.extend({
 	model: Item,
@@ -22,9 +30,9 @@ var Cell = Backbone.View.extend({
 	template: _.template($('#cell-template').html()),
 	
 	initialize: function() {
-		_.bindAll(this, 'render');
+		_.bindAll(this, 'render', 'remove');
 		this.model.bind('change', this.render);
-		this.model.bind('destroy', this.remove);
+		this.model.bind('remove', this.remove);
 		this.render();
 		this.$el.attr('id', this.model.get('_id'));
 	},
@@ -36,9 +44,10 @@ var Cell = Backbone.View.extend({
 
 var Grid = Backbone.View.extend({
 	initialize: function() {
-		_.bindAll(this, 'append', 'rank');
-		this.collection.bind('add', this.append);
+		_.bindAll(this, 'append', 'rank', 'clear');
+		this.clear();
 	},
+	
 	append: function(item) {
 		item.set({view: new Cell({model: item})});
 		this.$el.append(item.get('view').$el);
@@ -48,31 +57,89 @@ var Grid = Backbone.View.extend({
 			var rank = Math.floor(this.collection.getRank(item) * 4);
 			item.get('view').$el.addClass('span' + (rank + 2));
 		}.bind(this));
+	},
+	clear: function() {
+		this.collection = new Items;
+		this.collection.bind('add', this.append);
+		this.rank();
+	}
+});
+
+var Router = Backbone.Router.extend({
+	initialize: function(options) {
+		_.bindAll(this, 'clearPage');
+		this.grid = options.grid;
+		this.socket = options.socket;
+	},
+	
+	routes: {
+		'about': 'about',
+		'new': 'new',
+		'*path': 'front'
+	},
+	
+	clearPage: function() {
+		console.log('clearing');
+		_.each(this.grid.collection.models, function(item) {
+			item.remove();
+		});
+		this.grid.clear();
+	},
+	
+	about: function() {
+		this.clearPage();
+		this.current = 'about';
+	},
+	
+	'new': function() {
+		this.clearPage();
+		this.current = 'new';
+		this.socket.emit('get', 'new');
+	},
+	
+	front: function() {
+		console.log('getting front');
+		this.clearPage();
+		this.current = 'front';
+		this.page = 0;
+		this.socket.emit('get', 'front');
 	}
 });
 
 $(function() {
-	var items = new Items;
-
 	var grid = new Grid({
-		el: $('#grid'),
-		collection: items
+		el: $('#grid')
 	});
 	
 	var socket = io.connect();
+	
+	var router = new Router({grid: grid, socket: socket});
+	Backbone.history.start();
 	
 	socket.on('online', function(online) {
 		$('#online').text(online);
 	});
 	
-	socket.on('new', function(data) {
+	socket.on('posts', function(data) {
 		if(data.length) {
 			_.each(data, function(item) {
-				items.add(new Item(item));
+				grid.collection.add(new Item(item));
 			});
 		} else {
-			items.add(new Item(data));
+			grid.collection.add(new Item(data));
 		}
 		grid.rank();
+	});
+	
+	var lastScroll = Date.now();
+	$(window).scroll(function(e) {
+		console.log((Date.now() - lastScroll), $(window).scrollTop() + $(window).innerHeight() + 100, $(document).height());
+		if(router.current === 'front'
+		&& (Date.now() - lastScroll) > 2000
+		&& $(window).scrollTop() + $(window).innerHeight() > $(document).height() - 100) {
+			lastScroll = Date.now();
+			socket.emit('get','front.' +  ++router.page);
+			console.log('getting page', router.page);
+		}		
 	});
 });
